3661b651f4a5eadfa55b9f916939cd51ddcd87c9
LarsZauberer <wasser.ian@gmail.com>
2026-02-07 18:15:02 +0100

    Remove the main print statements

--- src/main.c
+++ src/main.c
@@ -5,22 +5,16 @@ int main() {
   TS_Scene_t *scene = ts_create_scene();
 
   int status = ts_load_plugin(scene, "build/libtheseed_components.so");
-  printf("Status: %d\n", status);
   status = ts_load_plugin(scene, "build/libtheseed_systems.so");
-  printf("Status: %d\n", status);
 
   size_t window = ts_add_entity(scene);
   status = ts_add_component(scene, window, "TS_Window");
-  printf("Status: %d\n", status);
 
   status = ts_add_system(scene, "ts_window_renderer", 100);
-  printf("Status: %d\n", status);
 
   status = ts_add_system(scene, "ts_window_quiter", 1000);
-  printf("Status: %d\n", status);
 
   status = ts_add_system(scene, "ts_window_reloader", 0);
-  printf("Status: %d\n", status);
 
   while (1) {
     ts_tick_scene(scene);
