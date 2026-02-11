aee9dad9f2768c0adf9f452bd62d731c2a68b1f1
LarsZauberer <wasser.ian@gmail.com>
2026-02-11 10:25:39 +0100

    Added a utils function to read a file to a gstring

--- include/TheSeed/core/utils.h
+++ include/TheSeed/core/utils.h
@@ -1,1 +1,5 @@
+#include <glib.h>
+
 char *ts_copy_char_ptr(const char *);
+
+int ts_read_file_to_gstring(const char *filepath, GString **out_string);
--- src/TheSeed/core/utils.c
+++ src/TheSeed/core/utils.c
@@ -1,5 +1,6 @@
 #include "TheSeed/core/utils.h"
 #include <stddef.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -8,4 +9,22 @@ char *ts_copy_char_ptr(const char *src) {
   char *out = (char *)malloc(sizeof(char) * (path_length + 1));
   strcpy(out, src);
   return out;
+}
+
+int ts_read_file_to_gstring(const char *filepath, GString **out_string) {
+  FILE *file = fopen(filepath, "r");
+  if (!file) {
+    fprintf(stderr, "Error: Failed to open file '%s'\n", filepath);
+    return 1;
+  }
+
+  *out_string = g_string_new(NULL);
+
+  char buffer[1024];
+  while (fgets(buffer, sizeof(buffer), file)) {
+    g_string_append(*out_string, buffer);
+  }
+
+  fclose(file);
+  return 0;
 }
