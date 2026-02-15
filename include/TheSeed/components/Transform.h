9f7a7aa05eec81ea67387ea13c4d3c2bc8acbc53
LarsZauberer <wasser.ian@gmail.com>
2026-02-15 03:10:56 +0100

    Added position transform reconstruction

--- include/TheSeed/components/Transform.h
+++ include/TheSeed/components/Transform.h
@@ -1,6 +1,7 @@
 #ifndef TS_Transform_H
 #define TS_Transform_H
 
+#include "TheSeed/ecs/Scene.h"
 #include "cglm/types.h"
 typedef struct {
   vec3 position;
@@ -10,5 +11,9 @@ typedef struct {
 
 void *ts_create_TS_Transform();
 void ts_destroy_TS_Transform(void *);
+void ts_serializer_TS_Transform(void *transform,
+                                TS_Serialization *serialization);
+void ts_deserialization_TS_Transform(void *transform,
+                                     TS_Serialization *serialization);
 
 #endif
--- src/TheSeed/components/Transform.c
+++ src/TheSeed/components/Transform.c
@@ -1,4 +1,5 @@
 #include "TheSeed/components/Transform.h"
+#include "cglm/types.h"
 #include "cglm/vec3.h"
 #include <stdlib.h>
 
@@ -20,5 +21,28 @@ void *ts_create_TS_Transform() {
 
 void ts_destroy_TS_Transform(void *t) {
   free(t);
+  return;
+}
+
+void ts_serialize_TS_Transform(void *t, TS_Serialization *serialization) {
+  TS_Transform_t *transform = (TS_Transform_t *)t;
+
+  vec3 *position = (vec3 *)malloc(sizeof(vec3));
+
+  (*position)[0] = transform->position[0];
+  (*position)[1] = transform->position[1];
+  (*position)[2] = transform->position[2];
+
+  ts_set_serialization(serialization, "position", sizeof(vec3), position);
+  return;
+}
+
+void ts_deserialize_TS_Transform(void *t, TS_Serialization *serialization) {
+  TS_Transform_t *transform = (TS_Transform_t *)t;
+
+  vec3 *position = (vec3 *)ts_get_serialization(serialization, "position");
+  transform->position[0] = (*position)[0];
+  transform->position[1] = (*position)[1];
+  transform->position[2] = (*position)[2];
   return;
 }
