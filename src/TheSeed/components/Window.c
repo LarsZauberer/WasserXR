8baf2b5781bcd1dbd6afea3ff9bf09f9f60090da
LarsZauberer <wasser.ian@gmail.com>
2026-02-22 13:22:16 +0100

    Add debug and release build options

--- src/TheSeed/components/Window.c
+++ src/TheSeed/components/Window.c
@@ -3,7 +3,6 @@
 #include "GL/gl.h"
 #include "TheSeed/components/Window.h"
 #include <GLFW/glfw3.h>
-#include <sanitizer/lsan_interface.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -11,9 +10,30 @@
 #define HEIGHT 600
 #define ANTIALIASING 8
 
+// LSAN fixes
+#if defined(__has_feature)
+#if __has_feature(address_sanitizer) || __has_feature(leak_sanitizer)
+#define HAVE_LSAN 1
+#endif
+#endif
+
+#if !defined(HAVE_LSAN)
+#if defined(__SANITIZE_ADDRESS__) || defined(__SANITIZE_LEAK__)
+#define HAVE_LSAN 1
+#endif
+#endif
+
+#ifdef HAVE_LSAN
+#include <sanitizer/lsan_interface.h>
+#define LSAN_DISABLE() __lsan_disable()
+#define LSAN_ENABLE() __lsan_enable()
+#else
+#define LSAN_DISABLE() ((void)0)
+#define LSAN_ENABLE() ((void)0)
+#endif
+
 static void setViewport(GLFWwindow *window, int width, int height) {
   glViewport(0, 0, width, height);
-  return;
 }
 
 void *ts_create_TS_Window() {
@@ -27,9 +47,9 @@ void *ts_create_TS_Window() {
   glfwWindowHint(GLFW_CONTEXT_CREATION_API, GLFW_EGL_CONTEXT_API);
   glfwWindowHint(GLFW_SAMPLES, ANTIALIASING);
 
-  __lsan_disable();
+  LSAN_DISABLE();
   this->window = glfwCreateWindow(WIDTH, HEIGHT, "TheSeed", NULL, NULL);
-  __lsan_enable();
+  LSAN_ENABLE();
 
   if (!this->window) {
     printf("Failed to create window");
@@ -53,12 +73,11 @@ void *ts_create_TS_Window() {
   return this;
 }
 
-void ts_destroy_TS_Window(void *w) {
-  TS_Window *this = (TS_Window *)w;
+void ts_destroy_TS_Window(void *window) {
+  TS_Window *this = (TS_Window *)window;
 
   if (this->window) {
     glfwDestroyWindow(this->window);
   }
-  free(w);
-  return;
+  free(window);
 }
