5861f7bb4c86e7f0b9a3d0a293c2df70900bcb49
LarsZauberer <wasser.ian@gmail.com>
2026-02-25 11:54:53 +0100

    Fix warnings in main.c

--- src/main.c
+++ src/main.c
@@ -15,28 +15,28 @@ int main() {
   // Create the ecs scene
   TS_Scene *scene = ts_create_scene();
 
-  int status = ts_load_plugin(scene, "build/libtheseed_components.so");
-  status = ts_load_plugin(scene, "build/libtheseed_systems.so");
+  ts_load_plugin(scene, "build/libtheseed_components.so");
+  ts_load_plugin(scene, "build/libtheseed_systems.so");
 
   size_t window = ts_add_entity(scene);
-  status = ts_add_component(scene, window, "TS_Window", NULL);
+  ts_add_component(scene, window, "TS_Window", NULL);
 
   size_t camera = ts_add_entity(scene);
-  status = ts_add_component(scene, camera, "TS_Camera", NULL);
-  status = ts_add_component(scene, camera, "TS_Transform", NULL);
+  ts_add_component(scene, camera, "TS_Camera", NULL);
+  ts_add_component(scene, camera, "TS_Transform", NULL);
 
   TS_Transform *camera_transform =
       ts_entity_get_component(scene, camera, "TS_Transform");
-  camera_transform->position[2] = 3.0f;
+  camera_transform->position[2] = 3.0F;
 
   size_t triangle = ts_add_entity(scene);
   ts_add_component(scene, triangle, "TS_Transform", NULL);
   ts_add_component(scene, triangle, "TS_Model", NULL);
   TS_Transform *triangle_transform =
       ts_entity_get_component(scene, triangle, "TS_Transform");
-  triangle_transform->position[0] = 0.0f;
-  triangle_transform->rotation[1] = 45.0f;
-  triangle_transform->rotation[2] = 45.0f;
+  triangle_transform->position[0] = 0.0F;
+  triangle_transform->rotation[1] = 45.0F;
+  triangle_transform->rotation[2] = 45.0F;
 
   TS_Model *triangle_model =
       ts_entity_get_component(scene, triangle, "TS_Model");
@@ -52,14 +52,14 @@ int main() {
   // triangle2_transform->position[0] = -1.0f;
 
   // Add the systems
-  status = ts_add_system(scene, "ts_window_pre_renderer", 50);
-  status = ts_add_system(scene, "ts_window_post_renderer", 150);
+  ts_add_system(scene, "ts_window_pre_renderer", 50);
+  ts_add_system(scene, "ts_window_post_renderer", 150);
 
-  status = ts_add_system(scene, "ts_window_quiter", 100);
+  ts_add_system(scene, "ts_window_quiter", 100);
 
-  status = ts_add_system(scene, "ts_window_reloader", 100);
+  ts_add_system(scene, "ts_window_reloader", 100);
 
-  status = ts_add_system(scene, "ts_mesh_renderer", 100);
+  ts_add_system(scene, "ts_mesh_renderer", 100);
 
   while (1) {
     ts_tick_scene(scene);
