6399b4e942cc711766bc34324a75842ef3a17b95
LarsZauberer <wasser.ian@gmail.com>
2026-03-13 15:39:40 +0100

    Fix Plugin Serialization Unittest

--- tests/test_ecs_serialize_plugin.c
+++ tests/test_ecs_serialize_plugin.c
@@ -12,6 +12,12 @@ struct TestCase {
   char *out;
 };
 
+static void free_case(void *ptr) {
+  TestCase *input = ptr;
+
+  ts_destroy_scene(input->scene);
+}
+
 static void unittest(const void *ptr) {
   const TestCase *input = ptr;
   char *data = ts_serialize_plugin(input->scene, input->plugin);
@@ -27,8 +33,9 @@ static void unittest(const void *ptr) {
   for (size_t i = 0; i < length; i++) {
     char byte_should = input->out[i];
     char byte_out = data[i];
-    ts_assert_test(byte_should == byte_out, "%c", byte_should, "%c", byte_out,
-                   "The Byte at index %d is not equal", i);
+    ts_assert_test(byte_should == byte_out, "Should: %d", byte_should,
+                   "Output: %d", byte_out, "The Byte at index %d is not equal",
+                   i);
   }
 
   free(data);
@@ -42,30 +49,33 @@ int main(int argc, char *argv[]) {
 
   // Constructing Cases
   TS_Scene *empty_scene = ts_create_scene();
+  TS_Scene *empty_scene2 = ts_create_scene();
 
   TS_Scene *plugin_scene = ts_create_scene();
   ts_assert(0 == ts_load_plugin(plugin_scene, "./libtheseed_components.so"),
             "Failed to load the plugin");
 
-  TestCase cases[] = {{NULL, NULL, 0, NULL},
-                      {NULL, "", 0, NULL},
-                      {empty_scene, "", 0, NULL},
-                      {empty_scene, "./libtheseed_components.so", 0, NULL},
-                      {plugin_scene, "", 0, NULL},
-                      {plugin_scene, "./libtheseed_components.so",
-                       strlen("./libtheseed_components.so") + sizeof(size_t),
-                       "\34\0\0\0\0\0\0\0./libtheseed_components.so"}};
+  TS_Scene *plugin_scene2 = ts_create_scene();
+  ts_assert(0 == ts_load_plugin(plugin_scene2, "./libtheseed_components.so"),
+            "Failed to load the plugin");
+
+  TestCase cases[] = {
+      {NULL, NULL, 0, NULL},
+      {NULL, "", 0, NULL},
+      {empty_scene, "", 0, NULL},
+      {empty_scene2, "./libtheseed_components.so", 0, NULL},
+      {plugin_scene, "", 0, NULL},
+      {plugin_scene2, "./libtheseed_components.so",
+       strlen("./libtheseed_components.so") + sizeof(size_t) + 1,
+       "\43\0\0\0\0\0\0\0./libtheseed_components.so\0"}};
 
   // Constructing Tests
 
   for (size_t i = 0; i < 6; i++) {
     char *path = g_strdup_printf("/theseed/test_ecs_add_component/%ld", i);
-    g_test_add_data_func(path, &cases[i], unittest);
+    g_test_add_data_func_full(path, &cases[i], unittest, free_case);
     free(path);
   }
-
-  ts_destroy_scene(empty_scene);
-  ts_destroy_scene(plugin_scene);
 
   return g_test_run();
 }
