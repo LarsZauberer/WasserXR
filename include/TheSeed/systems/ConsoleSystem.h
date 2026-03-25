9d0f6a0543c57fb290c74c3e0cf936e8149d14c3
LarsZauberer <wasser.ian@gmail.com>
2026-03-25 14:35:43 +0100

    Fix some errors regarding the const qualifiers

--- include/TheSeed/systems/ConsoleSystem.h
+++ include/TheSeed/systems/ConsoleSystem.h
@@ -10,6 +10,7 @@ void ts_attach_ts_console_system(TS_Scene *scene);
 void ts_detach_ts_console_system(TS_Scene *scene);
 void ts_system_ts_console_system(TS_Scene *scene, TS_Entity **entities,
                                  const size_t *groups);
-TS_System_Groups ts_select_ts_console_system(const TS_Scene *scene, const TS_Entity entity);
+TS_System_Groups ts_select_ts_console_system(const TS_Scene *scene,
+                                             TS_Entity entity);
 
 #endif
