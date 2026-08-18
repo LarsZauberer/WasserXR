28aa99edd14c49626f5de2a5d60e39fd074c770b
LarsZauberer <wasser.ian@gmail.com>
2026-08-18 22:40:39 +0200

    Ring: Add testing

--- src/utils/ring.rs
+++ src/utils/ring.rs
@@ -150,3 +150,78 @@ impl<T> Ring<T> {
         );
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use rstest::{fixture, rstest};
+
+    #[fixture]
+    fn basic() -> Ring<usize> {
+        Ring::new(2)
+    }
+
+    #[rstest]
+    #[case(&[], &[], 0)]
+    #[case(&[1], &[1], 1)]
+    #[case(&[1,2], &[1,2], 2)]
+    #[case(&[1,2,3], &[2,3], 2)]
+    #[case(&[1,2,3,4], &[3,4], 2)]
+    fn simple_cycle(
+        mut basic: Ring<usize>,
+        #[case] input: &[usize],
+        #[case] expected: &[usize],
+        #[case] expected_length: usize,
+    ) {
+        // Assert that adding and outputing is possible
+        input.iter().for_each(|x| basic.push(*x));
+        let output: Vec<usize> = basic.iter().cloned().collect();
+        assert_eq!(&output, expected);
+
+        // Assert capacity
+        assert_eq!(basic.cap(), 2);
+
+        // Assert length
+        assert_eq!(basic.len(), expected_length)
+    }
+
+    #[rstest]
+    fn simple_clear(mut basic: Ring<usize>) {
+        basic.push(1);
+        basic.clear();
+
+        // Assert basic properties
+        assert_eq!(basic.cap(), 2);
+        assert_eq!(basic.len(), 0);
+
+        // Assert that any output is also empty
+        let output: Vec<usize> = basic.iter().cloned().collect();
+        assert_eq!(&output, &[]);
+
+        let output = basic.get(0);
+        assert_eq!(output, None);
+    }
+
+    #[rstest]
+    #[case(&[])]
+    #[case(&[1])]
+    #[case(&[1,2])]
+    #[case(&[2,3])]
+    fn simple_mutate(mut basic: Ring<usize>, #[case] input: &[usize]) {
+        // Add the input
+        input.iter().for_each(|x| basic.push(*x));
+        let mut oracle = input.to_vec();
+
+        // Single modification
+        if let Some(value) = oracle.first_mut() {
+            *value += 1;
+            *basic.get_mut(0).unwrap() += 1;
+        }
+
+        // Mutate the whole ring
+        oracle.iter_mut().for_each(|x| *x += 1);
+        basic.iter_mut().for_each(|x| *x += 1);
+        let output: Vec<usize> = basic.iter().cloned().collect();
+        assert_eq!(output, oracle);
+    }
+}
