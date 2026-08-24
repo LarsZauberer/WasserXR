4a3dc41aa5b4164dcc8b2c8c19f56d8f73bcd6ce
LarsZauberer <wasser.ian@gmail.com>
2026-08-24 19:27:19 +0200

    rotation_buffer: Improved Test Cases

--- src/utils/rotation_buffer.rs
+++ src/utils/rotation_buffer.rs
@@ -133,23 +133,49 @@ mod test {
     #[case(&[1])]
     #[case(&[1,2,3])]
     fn test_lifecycle(mut rotator: VecRotationBuffer<usize>, #[case] input: &[usize]) {
+        assert_eq!(rotator.len(), 0);
         assert_eq!(rotator.as_slice(), &[]);
+        assert_eq!(rotator.get(0), None);
 
         // Have one half be filled before rotation and the other half after rotation
         let (left, right) = input.split_at(input.len() / 2);
 
         left.iter().for_each(|x| rotator.push(*x));
+        assert_eq!(rotator.len(), 0);
 
         rotator.rotate();
 
         right.iter().for_each(|x| rotator.push(*x));
 
+        assert_eq!(rotator.len(), left.len());
         assert_eq!(rotator.as_slice(), left);
         assert!(rotator.iter().eq(left.iter()));
+        if left.len() > 0 {
+            assert_eq!(*rotator.get(0).unwrap(), left[0]);
+        }
         rotator.rotate();
+        assert_eq!(rotator.len(), right.len());
         assert_eq!(rotator.as_slice(), right);
         assert!(rotator.iter().eq(right.iter()));
+        if left.len() > 0 {
+            assert_eq!(*rotator.get(0).unwrap(), right[0]);
+        }
         rotator.rotate();
+        assert_eq!(rotator.len(), 0);
+        assert_eq!(rotator.as_slice(), &[]);
+        assert_eq!(rotator.get(0), None);
+    }
+
+    #[rstest]
+    #[case(&[])]
+    #[case(&[1])]
+    #[case(&[1,2,3])]
+    fn test_clear(mut rotator: VecRotationBuffer<usize>, #[case] input: &[usize]) {
+        input.iter().for_each(|x| rotator.push(*x));
+
+        rotator.clear();
+        rotator.rotate();
+
         assert_eq!(rotator.as_slice(), &[]);
     }
 }
