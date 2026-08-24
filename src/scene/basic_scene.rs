54c6ef2c5f3aadc0f7e11039fd5e114789259494
LarsZauberer <wasser.ian@gmail.com>
2026-08-24 17:29:34 +0200

    basic_scene: Add simple tests

--- src/scene/basic_scene.rs
+++ src/scene/basic_scene.rs
@@ -27,3 +27,47 @@ where
         todo!()
     }
 }
+
+#[cfg(test)]
+mod slot_map_test {
+    use crate::entity::Entity;
+
+    use super::*;
+    use rstest::{fixture, rstest};
+    use slotmap::{DefaultKey, SlotMap};
+
+    struct MockEntity {}
+
+    impl Entity for MockEntity {}
+
+    type MockEntityID = DefaultKey;
+    type MockEntityStorage = SlotMap<MockEntityID, MockEntity>;
+    type MockScene = BasicScene<MockEntityStorage>;
+
+    #[fixture]
+    fn scene() -> MockScene {
+        MockScene {
+            entity_storage: SlotMap::<DefaultKey, MockEntity>::new(),
+        }
+    }
+
+    #[rstest]
+    fn test_add_remove(mut scene: MockScene) {
+        let entity = scene.add_entity();
+        scene
+            .remove_entity(entity)
+            .expect("Failed to remove the just created entity");
+        scene
+            .remove_entity(entity)
+            .expect_err("The removed entity can still be removed");
+    }
+
+    #[rstest]
+    fn test_reset(mut scene: MockScene) {
+        let entity = scene.add_entity();
+        scene.reset().expect("Failed to reset the scene");
+        scene
+            .remove_entity(entity)
+            .expect_err("The removed entity can still be removed");
+    }
+}
