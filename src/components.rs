41b85559daa912ead95caf3cca169a3debfd0149
LarsZauberer <wasser.ian@gmail.com>
2026-08-31 17:31:06 +0200

    component: Added simple Component record

--- src/components.rs
+++ src/components.rs
@@ -8,3 +8,28 @@ pub struct Component {
     manifest: ComponentManifest,
     data: *mut c_void,
 }
+
+impl Component {
+    pub fn new(plugin_id: PluginID, manifest: ComponentManifest) -> Self {
+        let data = unsafe { (manifest.creator)() };
+        Self {
+            plugin_id,
+            manifest,
+            data,
+        }
+    }
+
+    pub fn get_field(&self, name: &str) -> Option<*const c_void> {
+        todo!()
+    }
+
+    pub fn get_mut_field(&self, name: &str) -> Option<*mut c_void> {
+        todo!()
+    }
+}
+
+impl Drop for Component {
+    fn drop(&mut self) {
+        unsafe { (self.manifest.destroyer)(self.data) }
+    }
+}
