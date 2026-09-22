40150b9c7a43d44be4cfe301cbc25b4c5556f9b7
LarsZauberer <wasser.ian@gmail.com>
2026-09-22 18:54:19 +0200

    Field: Make AccessRequest Hashable

--- src/field.rs
+++ src/field.rs
@@ -3,7 +3,7 @@
 /// The lock requested for an entire component during a query callback.
 ///
 /// This controls access even though both modes expose `*mut c_void` pointers.
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum AccessRequest {
     /// Shared access. The callback must not write through the pointers.
     Read,
